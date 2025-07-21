858365b48118f90b0ba154688b437c51ea2a14e3
jamorant00 <138611160+jamorant00@users.noreply.github.com>
2025-07-21 13:52:00 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,4 @@
-const CACHE = 'v1';
-const FILES = [
-  '/',
-  '/index.html',
-  '/edit.html',
-  '/wrapped.html',
-  '/style.css',
-  '/script.js',
-  '/lor_tinky.jpg'
-];
-
-self.addEventListener('install', evt => {
-  evt.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-});
-
-self.addEventListener('fetch', evt => {
-  evt.respondWith(caches.match(evt.request).then(r => r || fetch(evt.request)));
-});
+const CACHE = 'v2';
+const FILES = ['/','/index.html','/edit.html','/wrapped.html','/style.css','/lor_tinky.jpg'];
+self.addEventListener('install', e=> e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('fetch',e=> e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
